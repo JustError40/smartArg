db84b2107368b5a48281966815284843d17f417f
JustErrore40 <f9d9e8@gmail.com>
2026-01-10 16:17:52 +0300

    core

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Chat, Message
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("title", "tg_chat_id", "chat_type", "created_at")
+    search_fields = ("title", "tg_chat_id")
+    list_filter = ("chat_type",)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("sender_name", "chat", "sender_role", "sent_at")
+    search_fields = ("text", "sender_name", "chat__title")
+    list_filter = ("sender_role", "chat__chat_type")
+    ordering = ("-sent_at",)
